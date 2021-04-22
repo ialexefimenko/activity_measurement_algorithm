ec958689c282e82cbff1e825767d131b05970221
ialexefimenko <ialexefimenko@gmail.com>
2021-04-22 15:24:34 +0300

    edit

---
+++ bin/mls.py
@@ -0,0 +1,11 @@
+import functions_LA as function 
+import coefficients 
+import numpy
+
+K=numpy.matrix([[coefficients.k11,coefficients.k12],[coefficients.k21,coefficients.k22]])
+
+R=numpy.matrix([[3.022],[0.430]])
+
+
+
+
